1c222f899cf83c5fad9b790820c99c88d0b4b3b1
erme07 <erik.medina07@gmail.com>
2023-10-20 13:55:23 -0300

    programando seccion tickets

--- js/script.js
+++ js/script.js
@@ -1,8 +1,70 @@
 const menuButton = document.querySelector(".burguer");
 const menu = document.getElementById("menu");
 const nav = document.getElementById("navBar");
+const ticketForm = document.getElementById("ticketForm");
+const amount = document.getElementById("cantidad");
+const selectCat = document.getElementById("selectCategory");
+const categoryStudent = document.querySelector(".category--student");
+const categoryTrainee = document.querySelector(".category--trainee");
+const categoryJunior = document.querySelector(".category--junior");
+const price = document.getElementById("total");
+
+const descuentos = {
+  estudiante: 80,
+  trainee: 50,
+  junior:15
+}
 
 let posicionY = 0;
+
+const discountPercent = () => {
+  if (Object.hasOwn(descuentos, selectCat.value)) 
+    return descuentos[selectCat.value]
+  else 
+    return 0;
+}
+
+const validateAmount = () => {
+  const amountValue = Number(amount.value);
+  if (Number.isInteger(amountValue) && amountValue >= 0) {
+    amount.classList.remove("invalid")
+    return true
+  } else {
+    amount.classList.add("invalid")
+    return false
+  }
+}
+
+const calculatePrice = () => {
+  let subtotal = amount.value * 200;
+  let total = subtotal - (subtotal * discountPercent() / 100)
+  return total;
+}
+
+const showPrice = () => {
+  if (validateAmount())
+    price.innerHTML = '$' + calculatePrice();
+  else
+    price.innerHTML = '$0';
+}
+
+const removeSelected=() => {
+  categoryStudent.classList.remove("selected");
+  categoryTrainee.classList.remove("selected");
+  categoryJunior.classList.remove("selected");
+}
+
+const selectCategory = () => {
+  removeSelected()
+  if (selectCat.value === 'estudiante') {
+    categoryStudent.classList.add("selected")
+  } else if (selectCat.value === 'trainee') {
+    categoryTrainee.classList.add("selected")
+  } else if (selectCat.value === 'junior') {
+    categoryJunior.classList.add("selected")
+  }
+}
+
 
 document.addEventListener("click", (e) => {
   if (e.target.matches(".menu__item")) {
@@ -17,13 +79,37 @@ document.addEventListener("click", (e) => {
     nav.classList.toggle("navBar--fixed");
     document.body.classList.toggle("navbarPlaceholder");
   }
+  if (e.target.id === 'clear') {
+    ticketForm.reset();
+    price.innerHTML = '$0';
+    selectCat.value = 'estudiante'
+    selectCategory()
+  }
+  if (e.target.id === 'resume') {
+    showPrice();
+  }
+  if (e.target.matches('.category--student') || e.target.matches('.category--student *')) {
+    selectCat.value = 'estudiante'
+    selectCategory()
+  }
+  if (e.target.matches('.category--trainee') || e.target.matches('.category--trainee *')) {
+    selectCat.value = 'trainee'
+    selectCategory()
+  }
+  if (e.target.matches('.category--junior') || e.target.matches('.category--junior *')) {
+    selectCat.value = 'junior'
+    selectCategory()
+  }
 })
 
 document.addEventListener('scroll', () => {
-  if (posicionY > scrollY) {
+  if (posicionY > scrollY) 
     nav.classList.add('navBar--sticky');
-  } else {
+  else 
     nav.classList.remove('navBar--sticky');
-  }
   posicionY = scrollY;
+})
+
+selectCat.addEventListener('change', () => {
+  selectCategory()
 })

1e47dd8d3d916930a364e841cda820a733fee762
erme07 <erik.medina07@gmail.com>
2023-11-05 23:20:14 -0300

    calculo descuento en categorias

--- js/tickets.js
+++ js/tickets.js
@@ -1,18 +1,19 @@
-const ticketForm = document.getElementById("ticketForm");
 const amount = document.getElementById("cantidad");
 const selectCat = document.getElementById("selectCategory");
 const categoryStudent = document.querySelector(".category--student");
 const categoryTrainee = document.querySelector(".category--trainee");
 const categoryJunior = document.querySelector(".category--junior");
+const categorySenior = document.querySelector(".category--senior");
 const price = document.getElementById("total");
+
+const COSTO_BOLETO = 200;
 
 const descuentos = {
   estudiante: 80,
   trainee: 50,
-  junior: 15
+  junior: 15,
+  senior: 0
 }
-
-
 
 const discountPercent = () => {
   if (Object.hasOwn(descuentos, selectCat.value))
@@ -21,34 +22,21 @@ const discountPercent = () => {
     return 0;
 }
 
-const validateAmount = () => {
-  const amountValue = Number(amount.value);
-  if (Number.isInteger(amountValue) && amountValue >= 0) {
-    amount.classList.remove("invalid")
-    return true
-  } else {
-    amount.classList.add("invalid")
-    return false
-  }
-}
-
 const calculatePrice = () => {
-  let subtotal = amount.value * 200;
+  let subtotal = amount.value * COSTO_BOLETO;
   let total = subtotal - (subtotal * discountPercent() / 100)
   return total;
 }
 
 const showPrice = () => {
-  if (validateAmount())
     price.innerHTML = '$' + calculatePrice();
-  else
-    price.innerHTML = '$0';
 }
 
 const removeSelected = () => {
   categoryStudent.classList.remove("selected");
   categoryTrainee.classList.remove("selected");
   categoryJunior.classList.remove("selected");
+  categorySenior.classList.remove("selected")
 }
 
 const selectCategory = () => {
@@ -59,19 +47,11 @@ const selectCategory = () => {
     categoryTrainee.classList.add("selected")
   else if (selectCat.value === 'junior')
     categoryJunior.classList.add("selected")
+  else if (selectCat.value === 'senior')
+    categorySenior.classList.add("selected")
 }
 
 document.addEventListener('click', (e) => {
-  if (e.target.id === 'clear') {
-    ticketForm.reset();
-    amount.classList.remove("invalid")
-    price.innerHTML = '$0';
-    selectCat.value = 'estudiante'
-    selectCategory()
-  }
-  if (e.target.id === 'resume') {
-    showPrice();
-  }
   if (e.target.matches('.category--student') || e.target.matches('.category--student *')) {
     selectCat.value = 'estudiante'
     selectCategory()
@@ -84,6 +64,8 @@ document.addEventListener('click', (e) => {
     selectCat.value = 'junior'
     selectCategory()
   }
+  if (e.target.matches('.category--senior') || e.target.matches('.category--senior *')) {
+    selectCat.value = 'senior'
+    selectCategory()
+  }
 })
-
-selectCat.addEventListener('change', () => selectCategory())
